4f36df6de64a0d41f792894aad34fa74ecd9d7f5
kimhg <kimhgedu@users.noreply.github.com>
2020-07-10 18:39:54 +0900

    [2020-07-10] update (vue-cli)

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-header :propsdata="str" v-on:renew="renewStr"/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  data: function() {
+    return {
+      str: 'hi',
+    }
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'Header'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>{{ propsdata }}</h1>
+    <button @click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+  // props: ['propsdata']
+  props: {
+    propsdata: String,
+  },
+  methods: {
+    sendEvent: function() {
+      this.$emit('renew');
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
